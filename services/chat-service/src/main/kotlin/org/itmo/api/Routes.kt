c43de7df07360cd0d8b293f69f503a5b5c88045f
trickyPupa <timofeylabor@gmail.com>
2026-01-15 11:30:53 +0300

    prometheus + grafana not working load.js script

--- services/chat-service/src/main/kotlin/org/itmo/api/Routes.kt
+++ services/chat-service/src/main/kotlin/org/itmo/api/Routes.kt
@@ -1,8 +1,10 @@
 package org.itmo.api
 
+import io.ktor.http.ContentType
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.micrometer.prometheus.PrometheusMeterRegistry
 import org.itmo.api.controllers.*
 import org.itmo.api.routes.*
 import org.itmo.service.AuditLogService
@@ -12,8 +14,10 @@ fun Application.configureRouting(
     chatController: ChatController,
     auditLogService: AuditLogService,
     testController: LoadTestController,
-    aiController: AiController
+    aiController: AiController,
+    prometheusRegistry: PrometheusMeterRegistry
 ) {
+
     routing {
         get("/") {
             call.respond(mapOf(
@@ -33,6 +37,10 @@ fun Application.configureRouting(
             } catch (e: Exception) {
                 call.respondError("error: ${e.message}")
             }
+        }
+
+        get("/metrics") {
+            call.respondText(prometheusRegistry.scrape(), ContentType.parse("text/plain; version=0.0.4"))
         }
 
         route("/api") {
